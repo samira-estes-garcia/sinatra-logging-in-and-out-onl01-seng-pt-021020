5dc97d5924bc951960463476ffe15c4b38252d8e
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-05-11 23:25:40 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -3,11 +3,11 @@ require 'sinatra/base'
 class Helpers
   
   def self.current_user(session) 
-    @user = User.find_by_id(session[:user.id])
+    @user = User.find_by_id(session[:user_id])
   end
   
   def self.is_logged_in?(session)
-    !! session[:user.id]
+    !! session[:user_id]
   end
   
 end
