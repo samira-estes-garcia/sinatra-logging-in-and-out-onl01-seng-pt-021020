2794a166ca19c938774f1e4eeb59049f4d689a94
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-05-11 23:22:50 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,11 @@
 class Helpers
+  
+  def self.current_user(sesh_hash) 
+    @user = User.find_by_id(session[:user.id])
+  end
+  
+  def self.is_logged_in?(sesh_hash)
+    !! session[:user.id]
+  end
+  
 end
